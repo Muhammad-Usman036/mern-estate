74187075a7b67327d4a13f2c0fd9ab80fce40dab
Muhammad-Usman036 <musmanmsadiq036@gmail.com>
2024-11-11 23:18:40 +0500

    create heder componenet

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Usman</span>
+            <span className="text-slate-700">E-state</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            type="text"
+            name=""
+            placeholder="Search ..."
+            id=""
+          />
+          <FaSearch />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to='signin'>
+          <li className="hidden sm:inline text-slate-700 hover:underline">
+            Signin
+          </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
